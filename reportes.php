c7445e85b12f58c0e956156c73fa5c145d0abd5e
robertsrodriguez859-lab <robertsrodriguez859@gmail.com>
2026-08-16 14:51:00 -0600

    Update reportes.php

--- reportes.php
+++ reportes.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['usuario_actual'])) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Reportes y Estadísticas - Sistema Iglesia</title>
+    <link rel="icon" type="image/png" href="Logo.png">
 <style>
   :root{
     --bg: #EEF1EC;
